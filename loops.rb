0689a19030d52a7faffa98ca0133adc1528b7e07
Ripster47 <mpolinski12@gmail.com>
2019-01-08 14:37:09 -0600

    initial commit

---
+++ loops.rb
@@ -0,0 +1,40 @@
+
+# array = ["jim", "sam", "jane", "spot", "Giraldo Mustafar"]
+
+
+# array.length.times do |index|
+
+#   dog = array[index]
+  
+#   p dog
+
+# end
+
+# puts "================="
+
+# array.each do |dog|
+
+#   p dog
+
+# end
+
+# count = 10
+
+# while count > 0
+  
+#   puts count
+
+#   count -= 1
+
+# end
+
+count = 10
+
+until count > 0
+  
+  puts count
+
+  count -= 1
+
+end
+
