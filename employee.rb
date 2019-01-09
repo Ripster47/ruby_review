c98072bc934a221576417461d6eab02f5318437a
Ripster47 <mpolinski12@gmail.com>
2019-01-09 10:43:54 -0600

    Manager addition

--- employee.rb
+++ employee.rb
@@ -20,6 +20,36 @@ class Employee
 
 end
 
+class Manager
+
+  attr_reader :first_name, :last_name, :active
+  attr_writer :active 
+
+  def initialize(input_options)
+    @first_name = input_options[:first_name]
+    @last_name = input_options[:last_name]
+    @salary = input_options[:salary]
+    @active = input_options[:active]
+  end
+
+  def print_info
+    puts "#{ @first_name } #{ @last_name } makes #{ @salary } a year." 
+  end
+
+  def give_annual_raise
+    @salary *= 1.05
+  end
+
+  def send_report
+    puts "Sending report..."
+    #code to send report
+    puts "Report Sent!"
+  end
+
+end
+
+
+
 employee_1 = Employee.new(
                           first_name: "Nick", 
                           last_name: "Cage", 
@@ -33,6 +63,18 @@ employee_2 = Employee.new(
                           salary: 80000, 
                           active: true
                           )
+
+manager = Manager.new(
+                      first_name: "Grace"
+                      last_name: "Hopper"
+                      salary: 100000
+                      active: true
+                      )
+
+
+
+
+
 
 employee_1.print_info
 employee_2.print_info
