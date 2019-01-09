7d9be1eef5116298eb7cc666435f796fe42b0291
Ripster47 <mpolinski12@gmail.com>
2019-01-09 10:49:12 -0600

    Manager inheritance

--- employee.rb
+++ employee.rb
@@ -20,25 +20,7 @@ class Employee
 
 end
 
-class Manager
-
-  attr_reader :first_name, :last_name, :active
-  attr_writer :active 
-
-  def initialize(input_options)
-    @first_name = input_options[:first_name]
-    @last_name = input_options[:last_name]
-    @salary = input_options[:salary]
-    @active = input_options[:active]
-  end
-
-  def print_info
-    puts "#{ @first_name } #{ @last_name } makes #{ @salary } a year." 
-  end
-
-  def give_annual_raise
-    @salary *= 1.05
-  end
+class Manager < Employee
 
   def send_report
     puts "Sending report..."
@@ -65,19 +47,16 @@ employee_2 = Employee.new(
                           )
 
 manager = Manager.new(
-                      first_name: "Grace"
-                      last_name: "Hopper"
-                      salary: 100000
+                      first_name: "Grace",
+                      last_name: "Hopper",
+                      salary: 100000,
                       active: true
                       )
 
-
-
-
-
-
 employee_1.print_info
 employee_2.print_info
+manager.print_info
+manager.send_report
 
 
   # def first_name  #reader method
