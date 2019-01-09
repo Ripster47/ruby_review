766ae91b08b4696c2e4a96d03a64deb94f7e0c64
Ripster47 <mpolinski12@gmail.com>
2019-01-09 09:15:35 -0600

    employees revised

---
+++ employee.rb
@@ -0,0 +1,5 @@
+employee_1 = {first_name: "Nick", last_name: "Cage", salary: 70000, active: true}
+employee_2 = {first_name: "Julia", last_name: "Andrews", salary: 80000, active: true}
+
+puts "#{employee_1[:first_name]} #{employee_1[:last_name]} makes #{employee_1[:salary]} a year."
+puts "#{employee_2[:first_name]} #{employee_2[:last_name]} makes #{employee_2[:salary]} a year."
